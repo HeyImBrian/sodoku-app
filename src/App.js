522e0ff5d25001f2f62e79c61cf53dfbfd8214f4
HeyImBrian <78251774+HeyImBrian@users.noreply.github.com>
2022-02-23 12:05:12 -0800

    Numbers are now filled in
    Needs checks to make sure that they fit sodoku rules

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+// TODO: Add highscore leaderboard based on time to solve. 
+
 function App() {
+  var sodokuMatrixRows = [[], [], [], [], [], [], [], [], []];
+
+  function getRandNum(min, max) {
+    let randNum = Math.floor(Math.random() * (max + 1 - min)) + min;
+
+
+    return randNum;
+  }
+
+
+
+
+    // Each row
+    for(let j = 0; j < 9; j++){
+      let numList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+      // Each index
+      for(let k = 1; k < 10; k++){
+
+        let randNum = getRandNum(0, k);
+        
+        sodokuMatrixRows[j].splice(randNum, 0, k);
+
+        console.log("ROW :   " + j);
+        console.log("Numlist =   " + numList);
+        console.log(sodokuMatrixRows[j]);
+        console.log("")
+      }
+    }
+
+  console.log(sodokuMatrixRows);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {sodokuMatrixRows[0]}<br/>
+          {sodokuMatrixRows[1]}<br/>
+          {sodokuMatrixRows[2]}<br/>
+          {sodokuMatrixRows[3]}<br/>
+          {sodokuMatrixRows[4]}<br/>
+          {sodokuMatrixRows[5]}<br/>
+          {sodokuMatrixRows[6]}<br/>
+          {sodokuMatrixRows[7]}<br/>
+          {sodokuMatrixRows[8]}<br/>
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
